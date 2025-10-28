f7f11e46d73353346c0bf0ab6d5fd3721c1f8b00
Khushal Sehrawat <133148875+khushalsehrawat@users.noreply.github.com>
2025-10-28 18:14:48 +0530

    Update SuemsApplication.java

--- src/main/java/com/suems/SuemsApplication.java
+++ src/main/java/com/suems/SuemsApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.suems.model.User;
+import com.suems.repository.UserRepository;
+
 @SpringBootApplication
 @EnableScheduling
 public class SuemsApplication {
